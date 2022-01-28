e2262bbe403475353a44e6191e4d6f5c0ecc30b6
GabrielIslas <2109084@upy.edu.mx>
2022-01-28 08:40:20 -0600

    Estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global variables
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "Gabriel Islas";
+char unCaracter = 'M';
+
+int main(){
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
