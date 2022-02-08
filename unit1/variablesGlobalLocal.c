b8fd7c5e803dbb0c6296438800cc8869a605e17f
GabrielIslas <2109084@upy.edu.mx>
2022-02-07 21:44:09 -0600

    testing new laptop

---
+++ unit1/variablesGlobalLocal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global variables
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "Gabriel Islas";
+char unCaracter = 'M';
+int temperatura;
+
+char* date = "02-02-22T10:25:00z";
+
+int main(){
+    return 0;
+}
