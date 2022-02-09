d1ec0b629f20c0cd8861972508f77d7776b7b728
GabrielIslas <2109084@upy.edu.mx>
2022-02-09 11:24:54 -0600

    argv argc object

--- unit1/argv_argc.c
+++ unit1/argv_argc.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 int main(int argc, char** argv){
+    if(argc <= 1) return 0;
     printf("Program executed: %s\n", argv[0]);
     printf("Number of inputs: %i\n", argc - 1);
     printf("Hola %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3]);
