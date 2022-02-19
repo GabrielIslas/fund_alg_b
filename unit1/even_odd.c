dcda2abefb3c33ab03f758e2c440b2c0bdd6c632
GabrielIslas <2109084@upy.edu.mx>
2022-02-19 10:46:12 -0600

    check number even or odd

---
+++ unit1/even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    if(argc != 2){
+        printf("Input one integer when executing the program\n");
+        return 0;
+    } 
+
+    int numberInput = atoi(argv[1]);
+
+    if(numberInput % 2 == 0){
+        printf("The number %d is even\n", numberInput);
+    }
+    else{
+        printf("The number %d is odd\n", numberInput);
+    }
+}
